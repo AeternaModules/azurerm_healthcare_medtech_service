92e266554f47e1aaa05c4810c4588ca330a5d0aa
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:57:43 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "healthcare_medtech_services" {
+  description = "All healthcare_medtech_service resources"
+  value       = azurerm_healthcare_medtech_service.healthcare_medtech_services
+}
+
